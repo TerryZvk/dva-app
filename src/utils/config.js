2b3672f81045c623748aa83f10c8cf8e3fc717eb
zhangwt <zhangwt@hollycrm.com>
2019-09-01 19:42:22 +0800

    api

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  API_PREFIX: process.env.NODE_ENV === 'develop' ? 'https://terryzvk-rails-api.herokuapp.com/api/v1' : 'http://localhost:3000/api/v1',
+  API_PREFIX: 'https://terryzvk-rails-api.herokuapp.com/api/v1', //prod
+  //API_PREFIX: 'http://localhost:3000/api/v1',       //dev
   openPages: ['/login']
 }
